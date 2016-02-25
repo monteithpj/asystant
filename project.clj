5a5e9b4ab4da8c31a958c0197b749d1e4ad75626
monteithpj <monteithpj@gmail.com>
2016-02-25 09:34:14 -0800

    Add auto tagging releases

--- project.clj
+++ project.clj
@@ -10,6 +10,14 @@
 
   :source-paths ["src/cljc"]
 
+  :release-tasks    [["vcs" "assert-committed"]
+                     ["change" "version" "leiningen.release/bump-version" "release"]
+                     ["vcs" "commit"]
+                     ["vcs" "tag"]
+                     ["change" "version" "leiningen.release/bump-version"]
+                     ["vcs" "commit"]
+                     ["vcs" "push"]]
+
   :aliases
   {"repl" ["with-profile" "+test" "repl"]}
   :profiles
